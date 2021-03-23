0f513cd8219298dd49998b2278e60d42d0563e23
Ivan Fedyanin <ivan.a.fedyanin@gmail.com>
2021-03-23 17:56:03 +0300

    removed SerializationUtils

--- src/androidMain/kotlin/com/dxc/ssi/agent/wallet/indy/helpers/WalletHelper.kt
+++ src/androidMain/kotlin/com/dxc/ssi/agent/wallet/indy/helpers/WalletHelper.kt
@@ -1,7 +1,10 @@
 package com.dxc.ssi.agent.wallet.indy.helpers
 
+import com.dxc.ssi.agent.wallet.indy.model.WalletConfig
+import com.dxc.ssi.agent.wallet.indy.model.WalletPassword
 import com.dxc.ssi.agent.wallet.indy.utils.EnvironmentUtils
-import com.dxc.ssi.agent.wallet.indy.utils.SerializationUtils
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.hyperledger.indy.sdk.wallet.Wallet
 import org.hyperledger.indy.sdk.wallet.WalletAlreadyOpenedException
 import org.hyperledger.indy.sdk.wallet.WalletExistsException
@@ -33,8 +36,8 @@ object WalletHelper {
      */
     @Throws(ExecutionException::class)
     fun createNonExisting(config: WalletConfig, password: WalletPassword) {
-        val walletConfigJson = SerializationUtils.anyToJSON(config)
-        val walletPasswordJson = SerializationUtils.anyToJSON(password)
+        val walletConfigJson = Json.encodeToString(config)
+        val walletPasswordJson = Json.encodeToString(password)
 
         Wallet.createWallet(walletConfigJson, walletPasswordJson).get()
     }
@@ -88,8 +91,8 @@ object WalletHelper {
         if (!exists(config.id))
             throw FileNotFoundException("Wallet ${EnvironmentUtils.getIndyWalletPath(config.id)} doesn't exist")
 
-        val walletConfigJson = SerializationUtils.anyToJSON(config)
-        val walletPasswordJson = SerializationUtils.anyToJSON(password)
+        val walletConfigJson = Json.encodeToString(config)
+        val walletPasswordJson = Json.encodeToString(password)
 
         return Wallet.openWallet(walletConfigJson, walletPasswordJson).get()
     }
@@ -133,37 +136,3 @@ object WalletHelper {
         = openOrCreate(WalletConfig(walletName), WalletPassword(walletPassword))
 }
 
-/**
- * {
- *     "id": string, Identifier of the wallet. Configured storage uses this identifier to lookup exact wallet data placement.
- *
- *     "storage_type": optional<string>, Type of the wallet storage. Defaults to 'default'.
- *     'Default' storage type allows to store wallet data in the local file.
- *     Custom storage types can be registered with indy_register_wallet_storage call.
- *
- *     "storage_config": optional<object>, Storage configuration json. Storage type defines set of supported keys.
- *     Can be optional if storage supports default configuration.
- *
- *     For 'default' storage type configuration is:
- *     {
- *         "path": optional<string>, Path to the directory with wallet files.
- *         Defaults to $HOME/.indy_client/wallets.
- *         Wallet will be stored in the file {path}/{id}/sqlite.db
- *     }
- * }
- */
-data class WalletConfig(
-    val id: String,
-    val storageType: String = "default",
-    val storageConfig: StorageConfig? = null
-)
-
-/**
- * Allows to define custom wallet storage path
- */
-data class StorageConfig(val path: String)
-
-/**
- * Represents wallet auth key
- */
-data class WalletPassword(val key: String)
