6c822322a2cf662a336152c2ccb1e621d4eeb9c5
Krzysztof Kamyczek <krzysztof.kamyczek@dxc.com>
2021-03-29 15:14:49 +0200

    Clean up and switching up to context

--- src/iosX64Test/kotlin/com/dxc/ssi/agent/api/impl/SimpleTest.kt
+++ src/iosX64Test/kotlin/com/dxc/ssi/agent/api/impl/SimpleTest.kt
@@ -10,33 +10,19 @@ typealias MyCallbackWallet = CPointer<CFunction<(indy_handle_t, indy_error_t) ->
 typealias MyCallback = CPointer<CFunction<(indy_handle_t, indy_error_t, CPointer<ByteVar>?, CPointer<ByteVar>?) -> Unit>>
 typealias MyCallbackWallet2 = CPointer<CFunction<(indy_handle_t, indy_error_t, indy_handle_t) -> Unit>>
 
-var wk: WeakReference<String> = WeakReference("empty")
-fun add(data: String?) {
-    //memScoped {
-    initRuntimeIfNeeded()
-    println("call " + data)
-    println("call " + data)
-    //val res: String? = data
-    //val arr = res!![0]
-    wk = WeakReference(data!!)
-    //}
-}
-
-var d: indy_handle_t = 0
-fun print(): String? {
-    return wk.get()
-}
-
 class IosIndyTest {
 
-    // #define levelMappings @{@"1": @"Error", @"2": @"Warning", @"3": @"Info", @"4": @"Debug", @"5": @"Trace"}
-    //    [IndyLogger setLogger:^(NSObject *context, NSNumber *level, NSString *target, NSString *message, NSString *modulePath, NSString *file, NSNumber *line) {
-    //    NSLog(@"%@    %@:%@ | %@", [levelMappings valueForKey:[NSString stringWithFormat:@"%@", level]], file, line, message);
-    //}];
     @Test
     fun test_indy_log() {
+        //val context: CValuesRef<*>? = null
+        val st = ""
+        //fun pass_string(str: CValuesRef<ByteVar /* = ByteVarOf<Byte> */>?)
+        //fun return_string(): CPointer<ByteVar /* = ByteVarOf<Byte> */>?
+        //fun copy_string(str: CValuesRef<ByteVar /* = ByteVarOf<Byte> */>?, size: Int): Int
 
-        val context: CValuesRef<*>? = null
+        val cString: CValues<ByteVarOf<Byte>> = st.cstr
+        val stableRef = StableRef.create(this)
+        val context: CValuesRef<*>?
         val enabledFn: CPointer<CFunction<(
             COpaquePointer?, indy_u32_t,
             CPointer<ByteVar>?
@@ -45,68 +31,52 @@ class IosIndyTest {
         val myExitCallback = staticCFunction(fun(
             log: CPointer<out CPointed>?,
             elem: indy_u32_t,
-            pointer: CPointer<ByteVarOf<Byte>>?,
+            pointer: CPointer<ByteVar>?,
             val1: CPointer<ByteVar>?,
             val2: CPointer<ByteVar>?,
             val3: CPointer<ByteVar>?,
-            number: indy_u32_t, /* = UInt */
+            number: indy_u32_t,
         ) {
-            println("log $log")
-            println("elem $elem")
-            println("pointer $pointer")
             println("val1 " + val1?.toKString())
             println("val2 " + val2?.toKString())
             println("val3 " + val3?.toKString())
-            println("number $number")
             return
         })
+
         val result = indy_set_logger(
-            context,
+            cString,
             enabledFn,
             myExitCallback,
             flushFn
         )
         println("indy_log $result")
-        sleep(10)
-    }
-    // #define levelMappings @{@"1": @"Error", @"2": @"Warning", @"3": @"Info", @"4": @"Debug", @"5": @"Trace"}
 
-    //    [IndyLogger setLogger:^(NSObject *context, NSNumber *level, NSString *target, NSString *message, NSString *modulePath, NSString *file, NSNumber *line) {
-//        NSLog(@"%@    %@:%@ | %@", [levelMappings valueForKey:[NSString stringWithFormat:@"%@", level]], file, line, message);
-//    }];
-//
-//    @Test
-//    fun test_indy_log() {
-//        val pointer = "119"
-//        var indy_get_logger: CValuesRef<*>? = null
-//        var enabledFn: CValuesRef<CPointerVar<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?) -> indy_bool_t /* = Boolean */>> /* = CPointerVarOf<CPointer<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?) → indy_bool_t /* = Boolean */>>> */>? =
-//            null
-//        var logFn: CValuesRef<CPointerVar<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, indy_u32_t /* = UInt */) -> Unit>> /* = CPointerVarOf<CPointer<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, indy_u32_t /* = UInt */) → Unit>>> */>? =
-//            null
-//        var flushFn: CValuesRef<CPointerVar<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */) -> Unit>> /* = CPointerVarOf<CPointer<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */) → Unit>>> */>? =
-//            null
-//
-//        val myExit_cb = staticCFunction(fun(
-//            indy_get_logger: CValuesRef<*>?,   //e: /Users/kkamyczek/ssi-mobile_final/ssi-mobile-sdk/src/iosX64Test/kotlin/com/dxc/ssi/agent/api/impl/SimpleTest.kt: (44, 25): type kotlinx.cinterop.CValuesRef<*>?  of callback parameter 1 is not supported here: doesn't correspond to any C type
-//            enabledFn: CValuesRef<CPointerVar<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?) -> indy_bool_t /* = Boolean */>> /* = CPointerVarOf<CPointer<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?) → indy_bool_t /* = Boolean */>>> */>?,
-//            logFn: CValuesRef<CPointerVar<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, indy_u32_t /* = UInt */) -> Unit>> /* = CPointerVarOf<CPointer<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */, indy_u32_t /* = UInt */, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, CPointer<ByteVar /* = ByteVarOf<Byte> */>?, indy_u32_t /* = UInt */) → Unit>>> */>?,
-//            flushFn: CValuesRef<CPointerVar<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */) -> Unit>> /* = CPointerVarOf<CPointer<CFunction<(COpaquePointer? /* = CPointer<out CPointed>? */) → Unit>>> */>?
-//        ) {
-//            println(indy_get_logger)
-//            println(enabledFn)
-//            println(logFn)
-//            println(flushFn)
-//            return
-//        })
-//        val result = indy_get_logger(
-//            indy_get_logger,
-//            enabledFn,
-//            logFn,
-//            flushFn
-//        )
-//        println("indy_create_wallet " + result)
-//        sleep(10)
-//    }
+        println(cString.toString())
+        sleep(10)
+        val command = 1
+        val pointer = "128"
+        val config = "{\"id\":\"testWalletName${pointer}\",\"storage_type\":\"default\"}"
+        val credentials = "{\"key\":\"testWalletPassword${pointer}\"}"
+        val myExit_cb: MyCallbackWallet = staticCFunction(fun(
+            xcommand_handle: indy_handle_t,
+            err: indy_error_t,
+        ) {
+            println("wallet")
+            println(xcommand_handle)
+            println(err)
+            return
+        })
+        val res: indy_error_t = indy_create_wallet(
+            command,
+            config,
+            credentials,
+            myExit_cb
+        )
+        println("indy_create_wallet " + res)
+        sleep(10)
+        println("indy_log $result")
+    }
+
     @Test
     fun run() {
         val didJson = "{}"
@@ -137,7 +107,39 @@ class IosIndyTest {
     @Test
     fun test_indy_create_wallet() {
         val command = 1
-        val pointer = "121"
+        val pointer = "127"
+        val context: CValuesRef<*>? = null
+        val enabledFn: CPointer<CFunction<(
+            COpaquePointer?, indy_u32_t,
+            CPointer<ByteVar>?
+        ) -> indy_bool_t>>? = null
+        val flushFn: CPointer<CFunction<(COpaquePointer?) -> Unit>>? = null
+        val myExitCallback = staticCFunction(fun(
+            log: CPointer<out CPointed>?,
+            elem: indy_u32_t,
+            pointer: CPointer<ByteVar>?,
+            val1: CPointer<ByteVar>?,
+            val2: CPointer<ByteVar>?,
+            val3: CPointer<ByteVar>?,
+            number: indy_u32_t, /* = UInt */
+        ) {
+            println("log ${log.rawValue}")
+            println("elem $elem")
+            println("pointer ${pointer?.toKString()}")
+            println("val1 " + val1?.toKString())
+            println("val2 " + val2?.toKString())
+            println("val3 " + val3?.toKString())
+            println("number $number")
+            return
+        })
+        val resultLog = indy_set_logger(
+            context,
+            enabledFn,
+            myExitCallback,
+            flushFn
+        )
+        println("indy_log $resultLog")
+        sleep(10)
         val config = "{\"id\":\"testWalletName${pointer}\",\"storage_type\":\"default\"}"
         val credentials = "{\"key\":\"testWalletPassword${pointer}\"}"
         val myExit_cb: MyCallbackWallet = staticCFunction(fun(
@@ -147,7 +149,6 @@ class IosIndyTest {
             println("wallet")
             println(xcommand_handle)
             println(err)
-//            d = xcommand_handle
             return
         })
         val result: indy_error_t = indy_create_wallet(
@@ -192,7 +193,6 @@ class IosIndyTest {
             println(verkey?.toKString())
             println(xcommand_handle)
             println(err)
-            add(did?.toKString())
             return
         })
         val exitCode: indy_error_t = indy_create_and_store_my_did(
@@ -202,7 +202,6 @@ class IosIndyTest {
             callback
         )
         sleep(10)
-        println(print())
         println("indy_create_and_store_my_did " + exitCode)
         test_indy_log()
     }
