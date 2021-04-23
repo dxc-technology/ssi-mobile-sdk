c927e42c5a6671a096e6237baa2e94e09459fe95
Ivan Fedyanin <ivan.a.fedyanin@gmail.com>
2021-04-23 19:27:33 +0300

    Added TODO

--- src/commonMain/kotlin/com/dxc/ssi/agent/utils/CoroutineHelper.kt
+++ src/commonMain/kotlin/com/dxc/ssi/agent/utils/CoroutineHelper.kt
@@ -7,6 +7,8 @@ expect class CoroutineHelper {
     companion object {
         //The reason this method exists is that we can not use runBlocking {} from common code in kotlin. This is kinda common wrapper for runBlocking
         fun <T> waitForCompletion(deferred: Deferred<T>):T
+
+        //TODO: currently this function might cause leaking threads as it requres closing the context. Understand where to close it properly
         fun  singleThreadCoroutineContext(threadName: String): CoroutineContext
     }
 }
