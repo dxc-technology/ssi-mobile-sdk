3bb5fc8041b109b7776f5599c6aa05c1ebbd8999
fedyiv <Ivan Fedyanin>
2021-04-28 18:13:03 +0300

    Post merge fixes

--- src/iosX64Test/kotlin/com/dxc/ssi/agent/api/impl/SsiAgentApiImplTest.kt
+++ src/iosX64Test/kotlin/com/dxc/ssi/agent/api/impl/SsiAgentApiImplTest.kt
@@ -2,42 +2,103 @@ package com.dxc.ssi.agent.api.impl
 
 import com.dxc.ssi.agent.api.callbacks.CallbackResult
 import com.dxc.ssi.agent.api.callbacks.didexchange.ConnectionInitiatorController
+import com.dxc.ssi.agent.api.callbacks.issue.CredReceiverController
+import com.dxc.ssi.agent.api.callbacks.verification.CredPresenterController
 import com.dxc.ssi.agent.didcomm.model.common.ProblemReport
 import com.dxc.ssi.agent.didcomm.model.didexchange.ConnectionRequest
 import com.dxc.ssi.agent.didcomm.model.didexchange.ConnectionResponse
 import com.dxc.ssi.agent.didcomm.model.didexchange.Invitation
+import com.dxc.ssi.agent.didcomm.model.issue.container.CredentialContainer
+import com.dxc.ssi.agent.didcomm.model.issue.container.CredentialOfferContainer
+import com.dxc.ssi.agent.didcomm.model.issue.container.CredentialRequestContainer
+import com.dxc.ssi.agent.didcomm.model.verify.container.PresentationRequestContainer
+import com.dxc.ssi.agent.ledger.indy.IndyLedgerConnector
+import com.dxc.ssi.agent.ledger.indy.IndyLedgerConnectorConfiguration
 import com.dxc.ssi.agent.model.Connection
 import com.dxc.ssi.agent.transport.Sleeper
-import com.dxc.ssi.agent.utils.ToBeReworked
 import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
-import kotlin.native.internal.test.testLauncherEntryPoint
 
 import kotlin.test.Test
 import kotlin.test.Ignore
-
+//TODO: move this test to common level
 class SsiAgentApiImplTest {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    @Ignore
+   // @Ignore
     fun basicTest() {
 
         // ToBeReworked.enableIndyLog()
 
         val ssiAgentApi = SsiAgentBuilderImpl()
             .withConnectionInitiatorController(ConnectionInitiatorControllerImpl())
+            .withCredReceiverController(CredReceiverControllerImpl())
+            .withCredPresenterController(CredPresenterControllerImpl())
+            .withLedgerConnector(IndyLedgerConnector(IndyLedgerConnectorConfiguration(genesisFilePath = "\"/home/ifedyanin/source/github/fedyiv/ssi-mobile-sdk-lumedic/files/docker_pool_transactions_genesis.txt\"")))
             .build()
 
         ssiAgentApi.init()
 
-        val invitationUrl =
-            "ws://192.168.0.117:7000/ws?c_i=eyJsYWJlbCI6Iklzc3VlciIsImltYWdlVXJsIjpudWxsLCJzZXJ2aWNlRW5kcG9pbnQiOiJ3czovLzE5Mi4xNjguMC4xMTc6NzAwMC93cyIsInJvdXRpbmdLZXlzIjpbIkRtMkhFRWNlWXo4cnJ1QTVMQWh0Y3B0WVFYVmN0N3V2NUVpNUxHTjdoY2h1Il0sInJlY2lwaWVudEtleXMiOlsiNjdxdnJTZVBSZ29qVGtNenZhcmV6OFZwNk1WTlV1U3d5UThDbWVHcnVKZUMiXSwiQGlkIjoiNTRiMjNmZDQtZWFiYy00YTcwLWIxM2UtYWI2Y2JkODg3MGQyIiwiQHR5cGUiOiJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiJ9"
-        ssiAgentApi.connect(invitationUrl)
 
+        val issuerInvitationUrl =
+            "ws://192.168.0.117:7000/ws?c_i=eyJsYWJlbCI6Iklzc3VlciIsImltYWdlVXJsIjpudWxsLCJzZXJ2aWNlRW5kcG9pbnQiOiJ3czovLzE5Mi4xNjguMC4xMTc6NzAwMC93cyIsInJvdXRpbmdLZXlzIjpbIjNVWmNBekVBU1JoNkRiaHhxU2U1Y1ZDRFlpUGFHZ2hCNmVMdzFVWmhjZXM0Il0sInJlY2lwaWVudEtleXMiOlsiQk03UDJibjRIbmdWWVNCNkJNSGJaR0RFUmJYNEFXdFVTR2thbWNna3pYVUwiXSwiQGlkIjoiYTRiMjU5MzEtYjJjMy00Y2Y1LWE0NjMtNmMyMjk5MWYyMzIwIiwiQHR5cGUiOiJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiJ9"
 
+        val verifierInvitationUrl =
+            "ws://192.168.0.117:9000/ws?c_i=eyJsYWJlbCI6IlZlcmlmaWVyIiwiaW1hZ2VVcmwiOm51bGwsInNlcnZpY2VFbmRwb2ludCI6IndzOi8vMTkyLjE2OC4wLjExNzo5MDAwL3dzIiwicm91dGluZ0tleXMiOlsiRUhoY1RkRE4xdFhlb0x2TTFGbXEzbkNjZmd0ZjZ4b3J2djJlbmJuVWd5NXgiXSwicmVjaXBpZW50S2V5cyI6WyJCak5VRTE0SzlIV3pkWVpSRWFjaTd5VEE4c3RqWmJxVm92cFpIZGh6Nm10QSJdLCJAaWQiOiIyZjI5Nzg4OC1mOTczLTRlYzEtOTEyMy02ZmFjZTg1ODkwZWEiLCJAdHlwZSI6ImRpZDpzb3Y6QnpDYnNOWWhNcmpIaXFaRFRVQVNIZztzcGVjL2Nvbm5lY3Rpb25zLzEuMC9pbnZpdGF0aW9uIn0="
 
-        Sleeper().sleep(100000)
+        println("Connecting to issuer")
+        ssiAgentApi.connect(issuerInvitationUrl)
+
+        //TODO: ensure that connection can be established without delay between two connections
+        //Sleeper().sleep(4000)
+
+        println("Connecting to verifier")
+        ssiAgentApi.connect(verifierInvitationUrl)
+
+        Sleeper().sleep(500000)
+
+    }
+
+    class CredPresenterControllerImpl : CredPresenterController {
+        override fun onRequestReceived(
+            connection: Connection,
+            presentationRequest: PresentationRequestContainer
+        ): CallbackResult {
+            return CallbackResult(true)
+        }
+
+        override fun onDone(connection: Connection): CallbackResult {
+            return CallbackResult(true)
+        }
+
+    }
+
+    class CredReceiverControllerImpl : CredReceiverController {
+        override fun onOfferReceived(
+            connection: Connection,
+            credentialOfferContainer: CredentialOfferContainer
+        ): CallbackResult {
+            return CallbackResult(true)
+        }
+
+        override fun onRequestSent(
+            connection: Connection,
+            credentialRequestContainer: CredentialRequestContainer
+        ): CallbackResult {
+            return CallbackResult(true)
+        }
+
+        override fun onCredentialReceived(
+            connection: Connection,
+            credentialContainer: CredentialContainer
+        ): CallbackResult {
+            return CallbackResult(true)
+        }
+
+        override fun onDone(connection: Connection, credentialContainer: CredentialContainer): CallbackResult {
+            return CallbackResult(true)
+        }
+
 
     }
 
@@ -66,7 +127,8 @@ class SsiAgentApiImplTest {
         }
 
         override fun onAbandoned(connection: Connection, problemReport: ProblemReport): CallbackResult {
-            TODO("Not yet implemented")
+            println("Connection abandoned : $connection")
+            return CallbackResult(true)
         }
 
     }
