917c858eafabe54ffceb8a6653e5306f8860c690
Krzysztof Kamyczek <krzysztof.kamyczek@dxc.com>
2021-06-24 15:22:53 +0200

    Fix an issue with test

--- src/androidTest/java/com/dxc/ssi/agent/api/impl/SsiAgentApiImplTest.kt
+++ src/androidTest/java/com/dxc/ssi/agent/api/impl/SsiAgentApiImplTest.kt
@@ -8,17 +8,23 @@ import com.dxc.ssi.agent.api.callbacks.CallbackResult
 import com.dxc.ssi.agent.api.callbacks.didexchange.ConnectionInitiatorController
 import com.dxc.ssi.agent.api.callbacks.issue.CredReceiverController
 import com.dxc.ssi.agent.api.callbacks.verification.CredPresenterController
-import com.dxc.ssi.agent.didcomm.model.common.ProblemReport
+import com.dxc.ssi.agent.api.pluggable.wallet.WalletManager
+import com.dxc.ssi.agent.api.pluggable.wallet.indy.IndyWalletConnector
 import com.dxc.ssi.agent.didcomm.model.didexchange.ConnectionRequest
 import com.dxc.ssi.agent.didcomm.model.didexchange.ConnectionResponse
 import com.dxc.ssi.agent.didcomm.model.didexchange.Invitation
 import com.dxc.ssi.agent.didcomm.model.issue.container.CredentialContainer
 import com.dxc.ssi.agent.didcomm.model.issue.container.CredentialOfferContainer
 import com.dxc.ssi.agent.didcomm.model.issue.container.CredentialRequestContainer
+import com.dxc.ssi.agent.didcomm.model.problem.ProblemReport
 import com.dxc.ssi.agent.didcomm.model.verify.container.PresentationRequestContainer
 import com.dxc.ssi.agent.ledger.indy.IndyLedgerConnector
 import com.dxc.ssi.agent.ledger.indy.IndyLedgerConnectorConfiguration
-import com.dxc.ssi.agent.model.Connection
+import com.dxc.ssi.agent.model.PeerConnection
+import com.dxc.ssi.agent.model.DidConfig
+import com.dxc.ssi.agent.wallet.indy.IndyWalletHolder
+import com.dxc.ssi.agent.wallet.indy.IndyWalletManager
+import com.dxc.utils.EnvironmentUtils
 import com.dxc.utils.Sleeper
 import org.junit.Ignore
 import org.junit.Rule
@@ -27,6 +33,9 @@ import org.junit.Test
 //TODO: if we can use some common kotlin tests to have common tests for all platforms
 class SsiAgentApiImplTest {
 
+    private val walletName = "newWalletName"
+    private val walletPassword = "newWalletPassword"
+    private val did = "Goci8gnhuC9vvxTWg1aFSx"
 
     @Rule
     @JvmField
@@ -46,7 +55,6 @@ class SsiAgentApiImplTest {
         )
     }
 
-
     @Test
     @Ignore("Ignored because it is actually integration test which should be moved out of unit tests in order to to run during build")
     //TODO: Move integration tests to separate module
@@ -55,12 +63,34 @@ class SsiAgentApiImplTest {
         val instrumentation = InstrumentationRegistry.getInstrumentation()
 
         println("Starting test")
+
+        EnvironmentUtils.initEnvironment(EnvironmentImpl(instrumentation.context))
+
+        val walletManager: WalletManager = IndyWalletManager
+
+        if (!walletManager.isWalletExistsAndOpenable(walletName, walletPassword))
+            walletManager.createWallet(walletName, walletPassword)
+
+        if (!walletManager.isDidExistsInWallet(did, walletName, walletPassword)) {
+            val didResult = walletManager.createDid(walletName = walletName, walletPassword = walletPassword)
+            print("Got generated didResult: did = ${didResult.did} , verkey = ${didResult.verkey}")
+            //Store did somewhere in your application to use it afterwards
+        }
+
+        val walletHolder = IndyWalletHolder(
+            walletName = walletName,
+            walletPassword = walletPassword,
+            didConfig = DidConfig(did = did)
+        )
+
         val indyLedgerConnectorConfiguration = IndyLedgerConnectorConfiguration(
             genesisMode = IndyLedgerConnectorConfiguration.GenesisMode.IP,
-            ipAddress = "192.168.0.122")
+            ipAddress = "192.168.0.122"
+        )
 
-        val ssiAgentApi = SsiAgentBuilderImpl()
-            .withEnvironment(EnvironmentImpl(instrumentation.context))
+        val indyWalletConnector = IndyWalletConnector.build(walletHolder)
+
+        val ssiAgentApi = SsiAgentBuilderImpl(indyWalletConnector)
             .withConnectionInitiatorController(ConnectionInitiatorControllerImpl())
             .withCredReceiverController(CredReceiverControllerImpl())
             .withCredPresenterController(CredPresenterControllerImpl())
@@ -71,19 +101,10 @@ class SsiAgentApiImplTest {
 
 
         val issuerInvitationUrl =
-            "ws://192.168.0.117:7000/ws?c_i=eyJsYWJlbCI6Iklzc3VlciIsImltYWdlVXJsIjpudWxsLCJzZXJ2aWNlRW5kcG9pbnQiOiJ3czovLzE5Mi4xNjguMC4xMTc6NzAwMC93cyIsInJvdXRpbmdLZXlzIjpbIkoxRVZQUVg0VzkyRjN0ajhUQVkzSEJGV3Myem85M29uVTliMWZ6OExiSzRhIl0sInJlY2lwaWVudEtleXMiOlsiNDZGdFlaVGRxajIyelNXYlR1WXd3R3U1dkJSWFl5UXR6TEsyUXdiQ2thZUsiXSwiQGlkIjoiZmVlMWIyNzUtNjQ0Ni00YjRlLWEwZjEtZDEyNDI0NzY3ZDYzIiwiQHR5cGUiOiJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiJ9"
-
-        val verifierInvitationUrl =
-            "ws://192.168.0.117:9000/ws?c_i=eyJsYWJlbCI6IlZlcmlmaWVyIiwiaW1hZ2VVcmwiOm51bGwsInNlcnZpY2VFbmRwb2ludCI6IndzOi8vMTkyLjE2OC4wLjExNzo5MDAwL3dzIiwicm91dGluZ0tleXMiOlsiRndORkdDOU1YdTNrOXhleFhQaDNOcHZaMkx3VVl2eDU2YnRzd0Z2MU0xc3MiXSwicmVjaXBpZW50S2V5cyI6WyJBc1BLNzZiMW1lWXVqWmI4aHhLdTV6RjlzamlIVE5Sa2tuakFSNnY3WmhFWSJdLCJAaWQiOiI0ZmQzYTBiZi1kZjNjLTQ1MTMtYTljMC05M2JkNmVjMmNlZDkiLCJAdHlwZSI6ImRpZDpzb3Y6QnpDYnNOWWhNcmpIaXFaRFRVQVNIZztzcGVjL2Nvbm5lY3Rpb25zLzEuMC9pbnZpdGF0aW9uIn0="
+            "ws://192.168.0.117:9000/ws?c_i=eyJsYWJlbCI6IkNsb3VkIEFnZW50IiwiaW1hZ2VVcmwiOm51bGwsInNlcnZpY2VFbmRwb2ludCI6IndzOi8vMTkyLjE2OC4wLjExNzo5MDAwL3dzIiwicm91dGluZ0tleXMiOlsiRVk0ZFZSUjZVb0Q5WWN5SkQ5VURZUmd6QnI3SDZFeEhGUTdxWEJkaHVKUXQiXSwicmVjaXBpZW50S2V5cyI6WyJIWWFCS3pGVVVWaTk2aGlkeFh4Q3RocWJSTVQyd3lyYm5OZTFCV1VUV3V6QyJdLCJAaWQiOiJhMGY4NjUwOC1kZDdjLTQxNGUtODA1Zi1kNGRhMTRhODc0YjUiLCJAdHlwZSI6ImRpZDpzb3Y6QnpDYnNOWWhNcmpIaXFaRFRVQVNIZztzcGVjL2Nvbm5lY3Rpb25zLzEuMC9pbnZpdGF0aW9uIn0="
 
         println("Connecting to issuer")
         ssiAgentApi.connect(issuerInvitationUrl)
-
-        //TODO: ensure that connection can be established without delay between two connections
-        //Sleeper().sleep(4000)
-
-        println("Connecting to verifier")
-        ssiAgentApi.connect(verifierInvitationUrl)
 
         Sleeper().sleep(500000)
 
@@ -91,44 +112,45 @@ class SsiAgentApiImplTest {
 
     class CredPresenterControllerImpl : CredPresenterController {
         override fun onRequestReceived(
-            connection: Connection,
+            connection: PeerConnection,
             presentationRequest: PresentationRequestContainer
         ): CallbackResult {
             return CallbackResult(true)
         }
 
-        override fun onDone(connection: Connection): CallbackResult {
+        override fun onDone(connection: PeerConnection): CallbackResult {
             return CallbackResult(true)
+        }
+
+        override fun onProblemReportGenerated(connection: PeerConnection, problemReport: ProblemReport) {
+
         }
 
     }
 
     class CredReceiverControllerImpl : CredReceiverController {
         override fun onOfferReceived(
-            connection: Connection,
+            connection: PeerConnection,
             credentialOfferContainer: CredentialOfferContainer
         ): CallbackResult {
             return CallbackResult(true)
         }
 
         override fun onRequestSent(
-            connection: Connection,
+            connection: PeerConnection,
             credentialRequestContainer: CredentialRequestContainer
         ): CallbackResult {
             return CallbackResult(true)
         }
 
         override fun onCredentialReceived(
-            connection: Connection,
+            connection: PeerConnection,
             credentialContainer: CredentialContainer
         ): CallbackResult {
             return CallbackResult(true)
         }
 
-        override fun onDone(
-            connection: Connection,
-            credentialContainer: CredentialContainer
-        ): CallbackResult {
+        override fun onDone(connection: PeerConnection, credentialContainer: CredentialContainer): CallbackResult {
             return CallbackResult(true)
         }
 
@@ -137,38 +159,28 @@ class SsiAgentApiImplTest {
 
     class ConnectionInitiatorControllerImpl : ConnectionInitiatorController {
         override fun onInvitationReceived(
-            connection: Connection,
-            endpoint: String,
+            connection: PeerConnection,
             invitation: Invitation
         ): CallbackResult {
             return CallbackResult(canProceedFurther = true)
         }
 
-        override fun onRequestSent(
-            connection: Connection,
-            request: ConnectionRequest
-        ): CallbackResult {
+        override fun onRequestSent(connection: PeerConnection, request: ConnectionRequest): CallbackResult {
             println("Request sent hook called : $connection, $request")
             return CallbackResult(true)
         }
 
-        override fun onResponseReceived(
-            connection: Connection,
-            response: ConnectionResponse
-        ): CallbackResult {
+        override fun onResponseReceived(connection: PeerConnection, response: ConnectionResponse): CallbackResult {
             println("Response received hook called : $connection, $response")
             return CallbackResult(true)
         }
 
-        override fun onCompleted(connection: Connection): CallbackResult {
+        override fun onCompleted(connection: PeerConnection): CallbackResult {
             println("Connection completed : $connection")
             return CallbackResult(true)
         }
 
-        override fun onAbandoned(
-            connection: Connection,
-            problemReport: ProblemReport
-        ): CallbackResult {
+        override fun onAbandoned(connection: PeerConnection, problemReport: ProblemReport): CallbackResult {
             println("Connection abandoned : $connection")
             return CallbackResult(true)
         }
