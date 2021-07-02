730cc5b2da0bcaf9679bdc9f7cfec9da293064db
Krzysztof Kamyczek <krzysztof.kamyczek@dxc.com>
2021-07-02 15:29:27 +0200

    Fix an app version

--- samples/swiftIosApp/swiftIosApp/swiftIosAppApp.swift
+++ samples/swiftIosApp/swiftIosApp/swiftIosAppApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 //@main
 struct swiftIosAppApp: App {
-    @available(iOS 13.0, *)
+    @available(iOS 14.0, *)
     var body: some Scene {
         WindowGroup {
             ContentView()
