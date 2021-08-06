e7822dbe8f9d0dc8d30b8c44c7f100965e05415c
Krzysztof Kamyczek <krzysztof.kamyczek@dxc.com>
2021-08-06 10:38:40 +0200

    Add use libraries

--- display_files.sh
+++ display_files.sh
@@ -1,2 +1,3 @@
-ls build/cocoapods/synthetic/IOS/kotlin_multiplatform_agent -la
+ls
+ls build/cocoapods/synthetic/IOS/kotlin_multiplatform_agent
 cat build/cocoapods/synthetic/IOS/kotlin_multiplatform_agent/CocoaPods.podfile.yaml
