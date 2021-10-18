df1a3cd8332ceca87ac6ad341a7c32ac56af84fa
Krzysztof Kamyczek <krzysztof.kamyczek@dxc.com>
2021-10-18 09:46:39 +0200

    Code refactor and clean up

--- src/jvmMain/kotlin/com/dxc/ssi/agent/transport/PlatformSocket.kt
+++ src/jvmMain/kotlin/com/dxc/ssi/agent/transport/PlatformSocket.kt
@@ -1,6 +1,8 @@
 package com.dxc.ssi.agent.transport
 
 
+import com.dxc.ssi.agent.kermit.Kermit
+import com.dxc.ssi.agent.kermit.LogcatLogger
 import okhttp3.*
 import okio.ByteString.Companion.encodeUtf8
 import org.eclipse.jetty.server.Server
@@ -8,12 +10,12 @@ import org.eclipse.jetty.servlet.ServletContextHandler
 import org.eclipse.jetty.websocket.api.Session
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage
 import org.eclipse.jetty.websocket.server.WebSocketUpgradeFilter
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse
 import org.eclipse.jetty.websocket.servlet.WebSocketCreator
-import sun.security.krb5.Confounder.bytes
 import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.nio.charset.StandardCharsets
@@ -24,29 +26,7 @@ private var pfl: PlatformSocketListener? = null
 internal actual class PlatformSocket actual constructor(url: String) {
     private val socketEndpoint = url
     private var webSocket: WebSocket? = null
-//    actual fun openSocket(platformSocketListener: PlatformSocketListener) {
-//        val socketRequest = Request.Builder().url(socketEndpoint).build()
-//        val webClient = OkHttpClient().newBuilder().build()
-//        webSocket = webClient.newWebSocket(
-//            socketRequest,
-//            object : WebSocketListener() {
-//                override fun onOpen(webSocket: WebSocket, response: Response) = platformSocketListener.onOpen()
-//                override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) =
-//                    platformSocketListener.onFailure(t)
-//
-//                override fun onMessage(webSocket: WebSocket, text: String) =
-//                    platformSocketListener.onMessage(text)
-//
-//                override fun onClosing(webSocket: WebSocket, code: Int, reason: String) =
-//                    platformSocketListener.onClosing(code, reason)
-//
-//                override fun onClosed(webSocket: WebSocket, code: Int, reason: String) =
-//                    platformSocketListener.onClosed(code, reason)
-//            }
-//        )
-//
-//
-//    }
+    private var runnable: Runnable? = null
 
     actual fun openSocket(platformSocketListener: PlatformSocketListener) {
         val socketRequest = Request.Builder().url(socketEndpoint).build()
@@ -69,7 +49,7 @@ internal actual class PlatformSocket actual constructor(url: String) {
             }
         )
         pfl = platformSocketListener
-        val runnable = Runnable {
+        runnable = Runnable {
             val isa = InetSocketAddress(InetAddress.getByName("192.168.0.104"), 8123)
             val server = Server(isa)
             val context = ServletContextHandler(ServletContextHandler.SESSIONS)
@@ -92,7 +72,7 @@ internal actual class PlatformSocket actual constructor(url: String) {
     actual fun closeSocket(code: Int, reason: String) {
         webSocket?.close(code, reason)
         webSocket = null
-
+        runnable = null
     }
 
     actual fun sendMessage(msg: String) {
@@ -100,33 +80,38 @@ internal actual class PlatformSocket actual constructor(url: String) {
         webSocket?.send(convertedMsg)
     }
 }
+
 @org.eclipse.jetty.websocket.api.annotations.WebSocket
 class JettyWebSocket {
+    private var logger: Kermit = Kermit(LogcatLogger())
+
     @OnWebSocketConnect
     fun onOpen(session: Session?) {
-        println("onOpen")
+        logger.d("JettyWebSocket") { "onOpen" }
         pfl?.onOpen()
     }
 
     @OnWebSocketMessage
-    fun onMessage(buf :ByteArray, offset:Int, lenght:Int) {
-        println("")
-        println(">>>>>>>>>>>>>")
-        println(buf)
-        println(offset)
-        println(lenght)
+    fun onMessage(buf: ByteArray, offset: Int, length: Int) {
         val message = String(buf, StandardCharsets.UTF_8)
-        print(message)
+        logger.d("JettyWebSocket") { message }
         pfl?.onMessage(message)
     }
 
     @OnWebSocketClose
     fun onClose(closeCode: Int, closeReasonPhrase: String?) {
-        println("onClose")
-        println("$closeCode $closeReasonPhrase")
-        pfl?.onClosed(closeCode,closeReasonPhrase!!)
+        logger.d("JettyWebSocket") { "onClose" }
+        logger.d("JettyWebSocket") { "$closeCode" + "$closeReasonPhrase" }
+        pfl?.onClosed(closeCode, closeReasonPhrase!!)
+    }
+
+    @OnWebSocketError
+    fun onError(error: Throwable) {
+        logger.d("JettyWebSocket") { "onError" }
+        pfl?.onFailure(error)
     }
 }
+
 class SocketCreator : WebSocketCreator {
     override fun createWebSocket(req: ServletUpgradeRequest, resp: ServletUpgradeResponse): Any {
         return JettyWebSocket()
